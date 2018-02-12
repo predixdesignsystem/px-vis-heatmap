ae9bab9564e7d6a0bc75a8ffe92a0a55550f9c5d
Sami Viitanen <sami.viitanen@vaadin.com>
2018-02-12 15:45:31 -0800

    Add test to notice cells left behind, improve labels

---
+++ test/internal-state-tests.js
@@ -0,0 +1,32 @@
+document.addEventListener('WebComponentsReady', () => {
+  describe('Internal state test', () => {
+    let chart;
+
+    beforeEach((done) => {
+      chart = fixture('px-vis-heatmap-fixture');
+
+      Polymer.RenderStatus.afterNextRender(chart, () => {
+        setTimeout(() => { // IE11
+          window.flush(done);
+        });
+      });
+    });
+
+    it('Right amount of cells, and not leaks', (done) => {
+      expect(chart !== undefined).to.be.eql(true);
+      expect(getCells(chart).length).to.be.eql(0);
+
+      chart.chartData = generateChartData(8, 8);
+
+      waitDrawUpdate(chart, () => {
+        expect(getCells(chart).length).to.be.eql(8 * 8);
+        chart.chartData = generateChartData(2, 2);
+
+        waitDrawUpdate(chart, () => {
+          expect(getCells(chart).length).to.be.eql(2 * 2);
+          done();
+        });
+      });
+    });
+  });
+});
